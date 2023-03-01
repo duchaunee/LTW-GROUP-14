2e7c9c7fadafbf3f5be5524e10679051aff57dc7
devper315 <phanvanthi315@gmail.com>
2023-03-01 15:10:14 +0700

    Phan Thi add project

---
+++ LTW-Group-14/src/main/java/com/org/group14/LtwGroup14Application.java
@@ -0,0 +1,13 @@
+package com.org.group14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LtwGroup14Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LtwGroup14Application.class, args);
+	}
+
+}
---
+++ LTW-Group-14/src/test/java/com/org/group14/LtwGroup14ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.group14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LtwGroup14ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

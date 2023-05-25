47d95285dc7649c86b2d592f863428246b54ad04
tempqeqas <40316966+quang125@users.noreply.github.com>
2023-05-25 20:32:48 +0700

    Update count and pagination CartItemDAO.java

--- LTW-GROUP-14/src/java/dao/CartItemDAO.java
+++ LTW-GROUP-14/src/java/dao/CartItemDAO.java
@@ -4,11 +4,11 @@
  */
 package dao;
 
-import connect.DBConnect;
-import entity.CartItem;
+import context.DBConnect;
+import entity.Order;
+import entity.OrderItem;
 import entity.Product;
-import java.sql.SQLException;
-import java.time.LocalDateTime;
+import entity.User;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,35 +16,64 @@ import java.util.List;
  *
  * @author Admin
  */
-public class CartItemDAO extends DAO{
+public class OrderItemDAO extends DAO{
+    private OrderDAO oDAO=new OrderDAO();
     private ProductDAO pDAO=new ProductDAO();
-    private CartDAO cDAO=new CartDAO();
-    public CartItemDAO(){}
-    public List<CartItem>findAll(){
-        List<CartItem>list=new ArrayList<>();
-        String query="select * from cart_item";
+    public OrderItemDAO(){}
+    public List<OrderItem>findAll(){
+        List<OrderItem>list=new ArrayList<>();
+        String query="select * from order_item";
         try{
             conn = new DBConnect().getConnection();
             ps=conn.prepareStatement(query);
             rs=ps.executeQuery();
             while(rs.next()){
-                list.add(new CartItem(rs.getInt(1), pDAO.findById(rs.getInt(2)), cDAO.findById(rs.getInt(3)) , 
-                        rs.getInt(4),rs.getTimestamp(5).toLocalDateTime()));
+                list.add(new OrderItem(rs.getInt(1),oDAO.findById(rs.getInt(3)),
+                                    pDAO.findById(rs.getInt(2)),rs.getString(4),rs.getInt(5)));
             }
         }catch(Exception e){
         }
         return list;
     }
-    public CartItem findById(Integer Id) throws Exception{
-        String query="select * from cart where id=?";
+    public List<OrderItem>pagningOrderItem(Integer id){
+        List<OrderItem>list=new ArrayList<>();
+        String query="select * from order_item order by id limit 5 offset ?";
+        try{
+            conn = new DBConnect().getConnection();
+            ps=conn.prepareStatement(query);
+            ps.setInt(1, (id-1)*5);
+            rs=ps.executeQuery();
+            while(rs.next()){
+                list.add(new OrderItem(rs.getInt(1),oDAO.findById(rs.getInt(3)),
+                                    pDAO.findById(rs.getInt(2)),rs.getString(4),rs.getInt(5)));
+            }
+        }catch(Exception e){
+        }
+        return list;
+    }
+    public int countOrderItem(){
+        String query="select count(*) from order_item";
+        try{
+            conn = new DBConnect().getConnection();
+            ps=conn.prepareStatement(query);
+            rs=ps.executeQuery();
+            while(rs.next()){
+                return rs.getInt(1);
+            }
+        }catch(Exception e){
+        }
+        return 0;
+    }
+    public OrderItem findById(Integer Id) throws Exception{
+        String query="select * from order_item where id=?";
         try{
             conn = new DBConnect().getConnection();
             ps=conn.prepareStatement(query);
             ps.setInt(1, Id);
             rs=ps.executeQuery();
             if(rs.next()){
-                return new CartItem(rs.getInt(1), pDAO.findById(rs.getInt(2)), cDAO.findById(rs.getInt(3)) , 
-                        rs.getInt(4),rs.getTimestamp(5).toLocalDateTime());
+                return new OrderItem(rs.getInt(1),oDAO.findById(rs.getInt(3)),
+                                    pDAO.findById(rs.getInt(2)),rs.getString(4),rs.getInt(5));
             }
             else{
             }

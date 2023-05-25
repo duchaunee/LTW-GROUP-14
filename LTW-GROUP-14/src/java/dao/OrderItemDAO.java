a7c1aaa97b314dc8942e56d109eaa3a18a8fcee6
tempqeqas <40316966+quang125@users.noreply.github.com>
2023-05-25 20:33:46 +0700

    Update count and pagination OrderItemDAO.java

--- LTW-GROUP-14/src/java/dao/OrderItemDAO.java
+++ LTW-GROUP-14/src/java/dao/OrderItemDAO.java
@@ -4,7 +4,7 @@
  */
 package dao;
 
-import connect.DBConnect;
+import context.DBConnect;
 import entity.Order;
 import entity.OrderItem;
 import entity.Product;
@@ -34,6 +34,35 @@ public class OrderItemDAO extends DAO{
         }catch(Exception e){
         }
         return list;
+    }
+    public List<OrderItem>pagningOrderItem(Integer id){
+        List<OrderItem>list=new ArrayList<>();
+        String query="select * from order_item order by id limit 5 offset ?";
+        try{
+            conn = new DBConnect().getConnection();
+            ps=conn.prepareStatement(query);
+            ps.setInt(1, (id-1)*5);
+            rs=ps.executeQuery();
+            while(rs.next()){
+                list.add(new OrderItem(rs.getInt(1),oDAO.findById(rs.getInt(3)),
+                                    pDAO.findById(rs.getInt(2)),rs.getString(4),rs.getInt(5)));
+            }
+        }catch(Exception e){
+        }
+        return list;
+    }
+    public int countOrderItem(){
+        String query="select count(*) from order_item";
+        try{
+            conn = new DBConnect().getConnection();
+            ps=conn.prepareStatement(query);
+            rs=ps.executeQuery();
+            while(rs.next()){
+                return rs.getInt(1);
+            }
+        }catch(Exception e){
+        }
+        return 0;
     }
     public OrderItem findById(Integer Id) throws Exception{
         String query="select * from order_item where id=?";

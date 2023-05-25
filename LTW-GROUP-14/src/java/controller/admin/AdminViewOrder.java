803cb257228b2b5809b82925071e0ded83445130
tempqeqas <40316966+quang125@users.noreply.github.com>
2023-05-25 20:26:17 +0700

    Add files via upload
    vieworder controller

---
+++ LTW-GROUP-14/src/java/controller/admin/AdminViewOrder.java
@@ -0,0 +1,82 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package controller;
+
+import dao.OrderItemDAO;
+import dao.ProductDAO;
+import entity.OrderItem;
+import entity.Product;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Admin
+ */
+@WebServlet(name = "AdminViewOrder", urlPatterns = {"/admin-vieworder"})
+public class AdminViewOrder extends HttpServlet {
+    private OrderItemDAO orderItemDAO=new OrderItemDAO();
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        request.setCharacterEncoding("utf-8");
+    } 
+
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        int size=orderItemDAO.countOrderItem();
+        int page=size/5+(size%5==0?0:1);
+        String indexPage=request.getParameter("index");
+        if(indexPage==null) indexPage="1";
+        int index=Integer.parseInt(indexPage);
+        List<OrderItem>orderList=orderItemDAO.pagningOrderItem(index);
+        request.setAttribute("orderList", orderList);
+        request.setAttribute("page", page);
+        request.setAttribute("size", size);
+        request.getRequestDispatcher("FE/Admin/viewOrder/viewOrder.jsp").forward(request, response);
+    } 
+
+    /** 
+     * Handles the HTTP <code>POST</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /** 
+     * Returns a short description of the servlet.
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+
+}

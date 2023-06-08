916c9d397c51914ffdd5feb684ad9defbb2867e4
tempqeqas <40316966+quang125@users.noreply.github.com>
2023-06-08 20:38:11 +0700

    Create Cart.js

---
+++ LTW-GROUP-14/web/FE/Cart/Cart.js
@@ -0,0 +1,28 @@
+function increaseQuantity(id, price, total) {
+  const currentQuantity = document.getElementById(id);
+  const newQuantity = parseInt(currentQuantity.innerHTML.trim()) + 1 < 10
+    ? `0${parseInt(currentQuantity.innerHTML.trim()) + 1}`
+    : parseInt(currentQuantity.innerHTML.trim()) + 1;
+  currentQuantity.innerHTML = newQuantity
+  // 
+  const priceEl = document.getElementById(price);
+  const totalEl = document.getElementById(total);
+  totalEl.innerText = parseInt(priceEl.innerHTML.trim()) * parseInt(newQuantity);
+}
+
+
+
+function decreaseQuantity(id, price, total) {
+  const currentQuantity = document.getElementById(id);
+  const currentValue = parseInt(currentQuantity.innerHTML.trim());
+  if (currentValue > 1) {
+    const newQuantity = currentValue - 1 < 10
+      ? `0${currentValue - 1}`
+      : currentValue - 1;
+    currentQuantity.innerHTML = newQuantity
+    // 
+    const priceEl = document.getElementById(price);
+    const totalEl = document.getElementById(total);
+    totalEl.innerText = parseInt(priceEl.innerHTML.trim()) * parseInt(newQuantity);
+  }
+}

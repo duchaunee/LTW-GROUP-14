fd75fbe7f9b8a375ba7948eb17119444ad8abc0c
longbi39 <long8and@gmail.com>
2023-06-09 18:55:09 +0700

    trang home

--- LTW-GROUP-14/src/java/dao/CartItemDAO.java
+++ LTW-GROUP-14/src/java/dao/CartItemDAO.java
@@ -33,7 +33,7 @@ public class CartItemDAO extends DAO{
         return list;
     }
     public CartItem findById(Integer Id){
-        String query="select * from cart where id=?";
+        String query="select * from cart_item where id=?";
         try{
             conn = new DBConnect().getConnection();
             ps=conn.prepareStatement(query);
@@ -106,4 +106,5 @@ public class CartItemDAO extends DAO{
         } catch (Exception e) {
         } 
     }
+   
 }
